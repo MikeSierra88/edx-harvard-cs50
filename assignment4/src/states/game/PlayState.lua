738ca4cecedf3a058384a65167ab645d427e8a35
MikeSierra <reklam11@gmail.com>
2020-05-18 14:09:04 +0200

    Assignment 4 finished

---
+++ assignment4/src/states/game/PlayState.lua
@@ -0,0 +1,178 @@
+--[[
+    GD50
+    Super Mario Bros. Remake
+
+    -- PlayState Class --
+]]
+
+PlayState = Class{__includes = BaseState}
+
+function PlayState:init()
+    self.camX = 0
+    self.camY = 0
+    self.background = math.random(3)
+    self.backgroundX = 0
+
+    self.tileMap = {}
+    self.level = {}
+
+    self.gravityOn = true
+    self.gravityAmount = 6
+
+    self.player = Player({
+        x = 0, 
+        y = 0,
+        width = 16, height = 20,
+        texture = 'green-alien',
+        stateMachine = StateMachine {
+            ['idle'] = function() return PlayerIdleState(self.player) end,
+            ['walking'] = function() return PlayerWalkingState(self.player) end,
+            ['jump'] = function() return PlayerJumpState(self.player, self.gravityAmount) end,
+            ['falling'] = function() return PlayerFallingState(self.player, self.gravityAmount) end
+        },
+        level = {},
+        tileMap = {}
+    })
+end
+
+function PlayState:enter(params)
+    if params then
+        self.player.score = params.score or 0
+        self.player.currentLevel = params.level or 1
+    end
+    print('Level ' .. self.player.currentLevel)
+    -- Every level adds 10 tiles to the level length
+    self.level = LevelMaker.generate(100 + ((self.player.currentLevel - 1) * 10), 10)
+    self.tileMap = self.level.tileMap
+    self.player.level = self.level
+    self.player.map = self.tileMap
+
+    -- reset key parameter
+    self.player.keyFound = false
+
+    -- Set player X to the first ground tile
+    self.player.x = self:findFirstGround()
+
+    self:spawnEnemies()
+
+    self.player:changeState('falling')
+end
+
+function PlayState:update(dt)
+    Timer.update(dt)
+
+    if love.keyboard.wasPressed('r') then
+        gStateMachine:change('start')
+    end
+
+    -- remove any nils from pickups, etc.
+    self.level:clear()
+
+    -- update player and level
+    self.player:update(dt)
+    self.level:update(dt)
+
+    -- constrain player X no matter which state
+    if self.player.x <= 0 then
+        self.player.x = 0
+    elseif self.player.x > TILE_SIZE * self.tileMap.width - self.player.width then
+        self.player.x = TILE_SIZE * self.tileMap.width - self.player.width
+    end
+
+    self:updateCamera()
+end
+
+function PlayState:render()
+    love.graphics.push()
+    love.graphics.draw(gTextures['backgrounds'], gFrames['backgrounds'][self.background], math.floor(-self.backgroundX), 0)
+    love.graphics.draw(gTextures['backgrounds'], gFrames['backgrounds'][self.background], math.floor(-self.backgroundX),
+        gTextures['backgrounds']:getHeight() / 3 * 2, 0, 1, -1)
+    love.graphics.draw(gTextures['backgrounds'], gFrames['backgrounds'][self.background], math.floor(-self.backgroundX + 256), 0)
+    love.graphics.draw(gTextures['backgrounds'], gFrames['backgrounds'][self.background], math.floor(-self.backgroundX + 256),
+        gTextures['backgrounds']:getHeight() / 3 * 2, 0, 1, -1)
+    
+    -- translate the entire view of the scene to emulate a camera
+    love.graphics.translate(-math.floor(self.camX), -math.floor(self.camY))
+    
+    self.level:render()
+
+    self.player:render()
+    love.graphics.pop()
+    
+    -- render score
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.setColor(0, 0, 0, 255/255)
+    love.graphics.print(tostring(self.player.score), 5, 5)
+    love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+    love.graphics.print(tostring(self.player.score), 4, 4)
+end
+
+function PlayState:updateCamera()
+    -- clamp movement of the camera's X between 0 and the map bounds - virtual width,
+    -- setting it half the screen to the left of the player so they are in the center
+    self.camX = math.max(0,
+        math.min(TILE_SIZE * self.tileMap.width - VIRTUAL_WIDTH,
+        self.player.x - (VIRTUAL_WIDTH / 2 - 8)))
+
+    -- adjust background X to move a third the rate of the camera for parallax
+    self.backgroundX = (self.camX / 3) % 256
+end
+
+--[[
+    Adds a series of enemies to the level randomly.
+]]
+function PlayState:spawnEnemies()
+    -- spawn snails in the level
+    for x = 1, self.tileMap.width do
+
+        -- flag for whether there's ground on this column of the level
+        local groundFound = false
+
+        for y = 1, self.tileMap.height do
+            if not groundFound then
+                if self.tileMap.tiles[y][x].id == TILE_ID_GROUND then
+                    groundFound = true
+
+                    -- random chance, 1 in 20
+                    if math.random(20) == 1 then
+                        
+                        -- instantiate snail, declaring in advance so we can pass it into state machine
+                        local snail
+                        snail = Snail {
+                            texture = 'creatures',
+                            x = (x - 1) * TILE_SIZE,
+                            y = (y - 2) * TILE_SIZE + 2,
+                            width = 16,
+                            height = 16,
+                            stateMachine = StateMachine {
+                                ['idle'] = function() return SnailIdleState(self.tileMap, self.player, snail) end,
+                                ['moving'] = function() return SnailMovingState(self.tileMap, self.player, snail) end,
+                                ['chasing'] = function() return SnailChasingState(self.tileMap, self.player, snail) end
+                            }
+                        }
+                        snail:changeState('idle', {
+                            wait = math.random(5)
+                        })
+
+                        table.insert(self.level.entities, snail)
+                    end
+                end
+            end
+        end
+    end
+end
+
+--[[
+    Finds first ground tile, returns its start x coordinate.
+    Checks every 16 tiles
+]]
+function PlayState:findFirstGround()
+    for x = 1, self.tileMap.width do
+        for y = 1, self.tileMap.height do
+            if self.tileMap.tiles[y][x].id == TILE_ID_GROUND then
+                return (self.tileMap.tiles[y][x].x - 1) * TILE_SIZE
+            end
+        end
+    end
+    return 1
+end
